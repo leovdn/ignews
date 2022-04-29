23fd32453b4159168be303d949c1900633492a1b
leovdn <leo_vdn@hotmail.com>
2022-04-29 14:37:41 -0300

    create stripe checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,11 +1,28 @@
+import { signIn, useSession } from "next-auth/client";
 import styles from "./styles.module.scss";
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    //Check user logged
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    //Create checkout session
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
