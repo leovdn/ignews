d648da3498232865578199e4465591c9a1c9559e
leovdn <leo_vdn@hotmail.com>
2022-05-07 00:30:55 -0300

    feat: integrate prismic prosts into /posts page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,10 +2,21 @@ import { GetStaticProps } from "next";
 import Head from "next/head";
 import { getPrismicClient } from "../../services/prismic";
 import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -14,32 +25,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de Maio de 2021</time>
-            <strong>Creating a Blog Post with Prismic</strong>
-            <p>
-              In this guide, you'll learn how to integrate Prismic CMS blog
-              posts
-            </p>
-          </a>
-
-          <a href="#">
-            <time>12 de Maio de 2021</time>
-            <strong>Creating a Blog Post with Prismic</strong>
-            <p>
-              In this guide, you'll learn how to integrate Prismic CMS blog
-              posts
-            </p>
-          </a>
-
-          <a href="#">
-            <time>12 de Maio de 2021</time>
-            <strong>Creating a Blog Post with Prismic</strong>
-            <p>
-              In this guide, you'll learn how to integrate Prismic CMS blog
-              posts
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -49,7 +41,7 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
-  const response = await prismic.query(
+  const response = await prismic.query<any>(
     [Prismic.predicates.at("document.type", "post")],
     {
       fetch: ["post.title", "post.content"],
@@ -57,9 +49,27 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
-  // console.log(response);
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-Br",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
